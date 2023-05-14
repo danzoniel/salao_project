e0839add58be4539300d247466fa253fdff79862
Daniel <daniel.de.s.sanches@gmail.com>
2023-05-14 00:33:01 -0300

    feat: adding responsivity

--- SalaoProject/src/main/java/com/mycompany/salaoproject/App.java
+++ SalaoProject/src/main/java/com/mycompany/salaoproject/App.java
@@ -1,19 +1,27 @@
 package com.mycompany.salaoproject;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 
 /**
@@ -25,24 +33,21 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        TextField textField = new TextField();
-        // textField.
-        // textField.getStyleClass().add("text-field");
-        textField.setPromptText("usuário");
+    
+        // AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("telalogin.fxml"));
+    
+        // Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+    
+        // stage.setWidth(screenBounds.getWidth());
+        // stage.setHeight(screenBounds.getHeight());
 
-        Image icon = new Image("https://www.flaticon.com/br/sticker-gratis/vaca_10776585?related_id=10776585");
-        ImageView imageView = new ImageView(icon);
-
-        // Using the ImageView with a Label
-        Label labelIcon = new Label("labelIcon", imageView);
-
-        VBox root = new VBox(loadFXML("telalogin"), labelIcon);
-
+        Scene scene = new Scene(loadFXML("telalogin"), 850, 550);
         stage.setTitle("Login");
-        scene = new Scene(root, 640, 426);
         stage.setScene(scene);
         stage.show();
     }
+    
+    
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
