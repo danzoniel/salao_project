6749c091d4c453ae0d13b8427f8ce40177f8a107
Daniel <daniel.de.s.sanches@gmail.com>
2023-05-21 13:50:32 -0300

    feat: telas cadastro prontas

--- SalaoProject/src/main/java/com/mycompany/salaoproject/ConfiguracoesController.java
+++ SalaoProject/src/main/java/com/mycompany/salaoproject/ConfiguracoesController.java
@@ -1,15 +1,10 @@
 package com.mycompany.salaoproject;
 
-import java.io.IOException;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 public class ConfiguracoesController {
 
